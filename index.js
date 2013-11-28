69dd88554ff16945299adbe57f55095c451d1bdf
Jason Kuhrt <jasonkuhrt@me.com>
2013-11-28 14:51:18 -0500

    Update to work with new remote-api-server component; Feature: automatically inform clients of the available API

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-var _ = require('lodash');
+var lo = require('lodash');
 var Emitter = require('emitter');
+var log = require('debug')('remote-api-client');
 
-var api = module.exports = {};
-var self = window;
+
+
+// Create a client of a remote-api
+var api = module.exports = function(uri, callback){
+
+  // Create an iframe to communicate with the remote-api
+  var iframe_el = window.document.createElement('iframe');
+  iframe_el.src = uri;
+  window.document.body.appendChild(iframe_el);
+
+
+  // Partially apply make_function for the new iframe
+  var make_function = lo.partialRight(api.make_function, iframe_el.contentWindow);
+
+  // Create api object
+  var emitter = api.make_client_emitter();
+
+
+  // Core api method to destroy DOM (thus connection) and event listeners
+  emitter.closeConnection = function(){
+    window.document.body.removeChild(iframe_el);
+    lo.each(['error', 'connection', 'disconnection'], function(e_name){ emitter.off(e_name); });
+  };
+
+
+  // Once the iframe is ready, ask
+  // the remote-api to expose itwindow
+
+  var on_iframe_ready = function(){
+    iframe_el.removeEventListener(on_iframe_ready);
+    log('iframe loaded');
+    make_function('get_remote_api')(function(err, api){
+      if (err) return console.error(err);
+      log('got remote-api api description:', api);
+      // Callback, with the remote-api exposed on api object
+      lo.extend(emitter, map_zip(api.functions, make_function))
+      callback(null, emitter);
+    });
+  };
+  iframe_el.addEventListener('load', on_iframe_ready);
+};
+
+
 
 api.make_function = function(command_name, peer){
   return function(){
-    var callback = _.last(arguments);
+    var callback = lo.last(arguments);
     var message = {
       type: 'function',
       name: command_name,
-      args: _.initial(arguments),
+      args: lo.initial(arguments),
       timestamp: Date.now()
     };
 
     peer.postMessage(message, '*');
-    self.addEventListener('message', returns_catcher);
+    window.addEventListener('message', returns_catcher);
 
     function returns_catcher(e){
       if (is_return(e.data, message)){
-        self.removeEventListener('message', returns_catcher);
+        window.removeEventListener('message', returns_catcher);
         callback.apply(null, e.data.returned);
       }
       // TODO timeout logic
@@ -30,8 +72,11 @@ api.make_function = function(command_name, peer){
       }
     }
   }
-}
+};
 
+
+
+// Trigger remote-api events
 api.make_host_emitter = function(peer, target_origin){
   return function(name, args){
     var message = {type:'event', name:name, args: args || []};
@@ -39,9 +84,13 @@ api.make_host_emitter = function(peer, target_origin){
   }
 }
 
+
+
+// Access remote-api events
 api.make_client_emitter = function(){
   var emitter = new Emitter();
-  self.addEventListener('message', function(e){
+
+  window.addEventListener('message', function(e){
     if (e.data.type === 'event'){
       emitter.emit.apply(emitter, [e.data.name].concat(e.data.args))
     }
@@ -49,15 +98,14 @@ api.make_client_emitter = function(){
   return emitter;
 };
 
-api.make_api = function(func_names, peer){
-  var emitter = api.make_client_emitter();
-  var functions = map_zip(func_names, _.partialRight(api.make_function, peer))
-  return _.extend(emitter, functions);
-};
+
+
+
+
 
 
 // Helpers
 
 function map_zip(collection, func){
-  return _.transform(collection, function(o, v){ o[v] = func(v); }, {});
+  return lo.transform(collection, function(o, v){ o[v] = func(v); }, {});
 }
